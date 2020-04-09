f477bcf12441a3e8e2b5cb3c67d14f7c04083307
DewiChantikaMaya06 <55245596+DewiChantikaMaya06@users.noreply.github.com>
2020-04-09 11:09:59 +0700

    Add files via upload
    bismillah

---
+++ Bismillah/src/sample/Controller/Dashboard.java
@@ -0,0 +1,49 @@
+package sample.Controller;
+
+
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+
+public class Dashboard {
+
+    @FXML
+    void clickArticle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickComment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickLove(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickReport(ActionEvent event) {
+
+    }
+
+    public void clickReport(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void clickArticle(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void clickComment(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void clickLove(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void clickHome(javafx.event.ActionEvent actionEvent) {
+    }
+}
