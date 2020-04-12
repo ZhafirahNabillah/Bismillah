36114f4a85a104d306a345c5ed68dd85369f827d
Dewi Chantika Maya <dewi.chantikamaya06@gmail.com>
2020-04-12 16:09:06 +0700

    ubah button gambar

---
+++ Bismillah/src/sample/Controller/Page_Profile.java
@@ -0,0 +1,43 @@
+package sample.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.awt.event.ActionEvent;
+
+public class Page_Profile {
+
+    @FXML
+    private Text txt_Name;
+
+    @FXML
+    private Text txt_Username;
+
+    @FXML
+    private TextField btnSearching;
+
+    @FXML
+    void clickArticle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickReport(ActionEvent event) {
+
+    }
+
+    public void clickHome(javafx.event.ActionEvent event) {
+    }
+
+    public void clickArticle(javafx.event.ActionEvent event) {
+    }
+
+    public void clickReport(javafx.event.ActionEvent event) {
+    }
+}
