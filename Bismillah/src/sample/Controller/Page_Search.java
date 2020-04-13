442e0e8a7dec2969318a6b125035e976b378054e
ZhafirahNabillah <55244721+ZhafirahNabillah@users.noreply.github.com>
2020-04-13 15:52:01 +0700

    page search update

--- Bismillah/src/sample/Controller/Page_Search.java
+++ Bismillah/src/sample/Controller/Page_Search.java
@@ -1,20 +1,4 @@
 package sample.Controller;
 
-import javafx.event.ActionEvent;
-
 public class Page_Search {
-    public void clickLove(ActionEvent event) {
-    }
-
-    public void clickComment(ActionEvent event) {
-    }
-
-    public void clickHome(ActionEvent event) {
-    }
-
-    public void clickArticle(ActionEvent event) {
-    }
-
-    public void clickReport(ActionEvent event) {
-    }
 }
